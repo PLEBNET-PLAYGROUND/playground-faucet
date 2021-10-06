e932391e028caf027e7312d5d849884719d04d87
Richard Safier <richardj@safier.com>
2021-10-06 18:50:50 -0400

    add graph endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, json
-from flask import request
+from flask import request,send_file,send_from_directory,abort
 import os
 
 api = Flask(__name__)
@@ -10,6 +10,13 @@ def get_faucet():
   commandline = 'bitcoin-cli -named sendtoaddress address="' + address + '" amount=1 fee_rate=1'
   os.system(commandline)
   return "Success"
+
+@api.route('/graph', methods=['GET'])
+def get_graph():
+  try:
+    return send_from_directory("/root/graph", filename="graph.json", as_attachment=False)
+  except FileNotFoundError:
+    abort(404)
 
 if __name__ == '__main__':
     api.run() 
